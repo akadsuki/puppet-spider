7659dadd4d88798f68bd41db8c792ae3e5dfb168
Andy Winarko <winarcooo@gmail.com>
2019-03-06 23:42:37 +0700

    get pricelist

--- src/spider.js
+++ src/spider.js
@@ -11,6 +11,13 @@ const puppeteer = require('puppeteer');
       .map((list) => list.innerText)
   );
 
+  const prices = await page.evaluate(() => 
+    Array.from(document.querySelectorAll('div.search_price'))
+      .map((list) => list.innerText
+      .replace(/(?:\r\n|\r|\n)/g, '|'))
+  );
+
+  console.log(prices);
   console.log(titles);
 
   await browser.close();
