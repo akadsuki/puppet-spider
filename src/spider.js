575a273587e5d98948aaed0ca7cd6854f79c7ee2
Andy Winarko <winarcooo@gmail.com>
2019-03-07 21:29:17 +0700

    get all page with recursion

--- src/spider.js
+++ src/spider.js
@@ -1,20 +1,41 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
+  const extractGameList = async url => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log(`scraping: ${url}`);
+
+    const gameListOnPage = await page.evaluate(() => 
+    Array.from(document.querySelectorAll('div.responsive_search_name_combined'))
+      .map(data => ({
+        title: data.querySelector('span.title').innerText.trim(),
+        price: data.querySelector('div.search_price').innerText.replace(/(?:\r\n|\r|\n)/g, '|')
+      }))
+    )
+    await page.close()
+  
+    if(gameListOnPage.length < 1) {
+      console.log(`terminate recursion on: ${url}`);
+      return gameListOnPage
+    } else {
+      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1
+      const nextUrl = `https://store.steampowered.com/search/?specials=1&page=${nextPageNumber}`;
+
+      return gameListOnPage.concat(await extractGameList(nextUrl))
+    }
+  }
+
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url = "https://store.steampowered.com/search/?specials=1";
-  await page.goto(url);
+  
+  const firstUrl = `https://store.steampowered.com/search/?specials=1&page=72`;
+  const GameList = await extractGameList(firstUrl)
 
-  const steamSales = await page.evaluate(() => ({
-    title: Array.from(document.querySelectorAll('span.title'))
-            .map((list) => list.innerText),
-    price: Array.from(document.querySelectorAll('div.search_price'))
-            .map((list) => list.innerText
-            .replace(/(?:\r\n|\r|\n)/g, '|'))
-  }));
-
-  console.log(steamSales);
+  fs.writeFile(`game_list.txt`, JSON.stringify(GameList), err => {
+    if (err) throw err;
+  })
+  console.log(`save at: game_list.txt`);
 
   await browser.close();
 })();
