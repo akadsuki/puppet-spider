141a2609826ee672e2b1053de2cdb22503ce27c0
Andy Winarko <winarcooo@gmail.com>
2019-03-07 14:17:39 +0700

    siplify page evaluate

--- src/spider.js
+++ src/spider.js
@@ -6,19 +6,15 @@ const puppeteer = require('puppeteer');
   const url = "https://store.steampowered.com/search/?specials=1";
   await page.goto(url);
 
-  const titles = await page.evaluate(() => 
-    Array.from(document.querySelectorAll('span.title'))
-      .map((list) => list.innerText)
-  );
+  const steamSales = await page.evaluate(() => ({
+    title: Array.from(document.querySelectorAll('span.title'))
+            .map((list) => list.innerText),
+    price: Array.from(document.querySelectorAll('div.search_price'))
+            .map((list) => list.innerText
+            .replace(/(?:\r\n|\r|\n)/g, '|'))
+  }));
 
-  const prices = await page.evaluate(() => 
-    Array.from(document.querySelectorAll('div.search_price'))
-      .map((list) => list.innerText
-      .replace(/(?:\r\n|\r|\n)/g, '|'))
-  );
-
-  console.log(prices);
-  console.log(titles);
+  console.log(steamSales);
 
   await browser.close();
 })();
